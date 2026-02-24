7105b5f15a0c34817de4362ada8b2003074b0e13
pobed1tel <s1mpleegamee@gmail.com>
2026-02-24 17:15:41 +0700

    Update task1.c

--- part1/task1.c
+++ part1/task1.c
@@ -4,3 +4,34 @@ int main(){
     printf("Привет, мир!\n");
     return 0;
 }
+
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+    char *buff = NULL;
+    size_t capacity = 524288;
+
+    buff = getline(&buff, &capacity, stdin);
+    
+    char *ptr = strok(buff, " ");
+
+    int* numptr = NULL;
+    int* newnumptr = numptr;
+    for(int i = 0; myPtr != NULL; i++) {
+        
+        // numptr[i] = sscanf(myPtr, "%d", &temp);
+        newnumptr = realloc(newnumptr, sizeof newnumptr + sizeof int);
+        newnumptr[i] = sscanf(myPtr, "%d", &temp);
+        myPtr = strtok(NULL, " ");
+
+        printf("%d ", newnumptr[i]);
+    }
+
+    printf("\n");
+
+
+    
+    return 0;
+}
